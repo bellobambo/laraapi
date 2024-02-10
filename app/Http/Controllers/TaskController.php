61cc75b97cd4c846807e765c17b4f1e054f58759
bellobambo <bellobambo21@gmail.com>
2024-02-10 09:51:25 +0100

    api sanctum

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Http\Resources\TaskCollection;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
@@ -15,13 +17,24 @@ class TaskController extends Controller
     }
 
 
-    public function store(Request $request, Task $task)
+    public function store(StoreTaskRequest $request, Task $task)
     {
-        $validated = $request->validate([
-            // 'title' => 'required|max:225'
-        ]);
+        $validated = $request->validated();
         $task = Task::create($validated);
 
         return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task->update($validated);
+        return new TaskResource($task);
+    }
+    public function destroy(Request $request, Task $task)
+    {
+      $task->delete();
+      return  response()->noContent();
     }
 }
