eab2ffa62040fbb6bd1eefab66781f1d9e36d951
bellobambo <bellobambo21@gmail.com>
2024-02-11 19:38:02 +0100

    sanctum auth

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -27,5 +29,11 @@ class Task extends Model
     public function creator(): BelongsTo
     {
         return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    protected static function booted() : void{
+        static::addGlobalScope('creator' , function (Builder $builder){
+            $builder->where( 'created_id' , Auth::id());
+        });
     }
 }
